5fe5203a75d6e2f322a1b7acc1828a037d00bfaa
DevTiw37 <tiwaridevesh937@gmail.com>
2023-07-04 19:29:02 +0530

    productivity v2.0

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-document.getElementById('motivationForm').addEventListener('submit', function(e) {
+document.getElementById('productivityForm').addEventListener('submit', function(e) {
   e.preventDefault();
   
-  var motivationLevel = document.getElementById('motivationLevel').value;
+  var productivityLevel = document.getElementById('productivityLevel').value;
   var currentDate = new Date().toLocaleDateString();
-  var motivationEntry = {
-    level: motivationLevel,
+  var productivityEntry = {
+    level: productivityLevel,
     date: currentDate
   };
   
-  addMotivationEntry(motivationEntry);
+  addProductivityEntry(productivityEntry);
 });
 
-function addMotivationEntry(entry) {
+function addProductivityEntry(entry) {
   var entryElement = document.createElement('li');
   entryElement.textContent = entry.date + ' - ' + entry.level;
   
-  document.getElementById('motivationList').appendChild(entryElement);
+  document.getElementById('productivityList').appendChild(entryElement);
   
   calculateDuration();
 }
 
 function calculateDuration() {
-  var entries = document.getElementById('motivationList').getElementsByTagName('li');
+  var entries = document.getElementById('productivityList').getElementsByTagName('li');
   
   if (entries.length > 1) {
     var firstEntryDate = new Date(entries[0].textContent.split(' - ')[0]);
     var lastEntryDate = new Date(entries[entries.length - 1].textContent.split(' - ')[0]);
     var duration = Math.round((lastEntryDate - firstEntryDate) / (1000 * 60 * 60 * 24));
     
-    document.getElementById('motivationEntries').setAttribute('data-duration', duration);
+    document.getElementById('productivityEntries').setAttribute('data-duration', duration);
   }
 }
